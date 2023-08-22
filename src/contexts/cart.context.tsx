321d429797e1dca7afa7aecb0064afd75b63fa4d
murilo barbosa <murilobarbosadarosa2002@gmail.com>
2023-08-22 19:30:58 -0300

    Feat: handle multiple products  of the same id on cart

--- src/contexts/cart.context.tsx
+++ src/contexts/cart.context.tsx
@@ -30,6 +30,19 @@ const CartContextProvider: FunctionComponent<CartContextProps> = ({
   }
 
   const addProductToCart = (product: Product) => {
+    const productIsAlreadyInCart = products.some(
+      (item) => item.id === product.id
+    )
+
+    if (productIsAlreadyInCart) {
+      return setProducts((products) =>
+        products.map((item) =>
+          item.id === product.id
+            ? { ...item, quantity: item.quantity + 1 }
+            : item
+        )
+      )
+    }
     setProducts((prevState) => [...prevState, { ...product, quantity: 1 }])
   }
 
